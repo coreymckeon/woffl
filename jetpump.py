598f32ab017bad2ac36f7f8cce202a553a6747b8
Kaelin Ellis <ellis.kaelin@gmail.com>
2023-09-13 13:23:34 -0800

    Moved E-42 data into tests.py

--- jetpump.py
+++ jetpump.py
@@ -1,5 +1,6 @@
-import numpy as np
 import math
+
+import numpy as np
 
 
 class JetPump:
@@ -129,7 +130,7 @@ class JetPump:
             print(f'Area Ratio {self.rat_ar} not recognized')
 
         # calculate throat diameter
-        dthr = math.sqrt(4*athr/3.1415)  # inches
+        dthr = math.sqrt(4*self.athr/3.1415)  # inches
         self.dthr = round(dthr, 5)  # inches
 
     def __str__(self):
@@ -209,6 +210,7 @@ def anlr_dims(tube_od, case_id):
     case_ia = 3.1415*case_id**2/(4*144)  # ft^2, casing cross area inside
 
     # outer perimeters
+    # I should delete this since it is no longer needed?
     tube_op = 3.1415*tube_od/12  # ft
     case_ip = 3.1415*case_id/12  # ft
 
@@ -285,39 +287,3 @@ def dp_pipe(fpipe, lpipe, rho, vel, dhyd):
     dp_p = round(dp_p, 2)
 
     return dp_p
-
-
-tube_od = 4.5  # inch
-case_id = 6.875  # inch
-
-jpump_md = 6693  # feet
-jpump_tvd = 4096.8  # feet, interpolated off well profile
-
-bpd_pf = 3500  # bwpd, power fluid rate
-d_pf = 62.4  # lbm/ft3
-u_pf = 1  # cp, viscosity of power fluid
-ppf_surf = 3046  # psi, power fluid surf pressure
-
-e42jetpump = JetPump('13', 'B')
-
-anlr_a, anlr_dhyd = anlr_dims(tube_od, case_id)
-q_pf = bpd_to_ft3s(bpd_pf)
-
-v_ann = round(q_pf/anlr_a, 2)  # ft/s annular velocity
-print(f'Annular Velocity: {v_ann} ft/s')
-
-rey = reynolds(d_pf, v_ann, anlr_dhyd, u_pf)
-print(f'Reynolds Number {rey}')
-f_ann = serghide(0.001, rey)
-print(f'Friction Factor {f_ann}')
-# add functionality of turbulent flow or not?
-dp_ann = dp_pipe(f_ann, jpump_md, d_pf, v_ann, anlr_dhyd)
-print(f'Friction Drop in Annulus {dp_ann} psi')
-
-p_st = static_press(d_pf, jpump_tvd)
-p_pf = ppf_surf + p_st - dp_ann
-
-print(f'Surf PF Pressure {ppf_surf} psi')
-print(f'Pump PF Pressure {round(p_pf,0)} psi')
-
-e42jetpump.p_jt(q_pf, p_pf, d_pf)
--- tests.py
+++ tests.py
@@ -1,3 +1,38 @@
-# need to test code on MPE-42
+from jetpump import *
 
-print('hello')
+# testing the jet pump code on E-42
+
+tube_od = 4.5  # inch
+case_id = 6.875  # inch
+
+jpump_md = 6693  # feet
+jpump_tvd = 4096.8  # feet, interpolated off well profile
+
+bpd_pf = 3500  # bwpd, power fluid rate
+d_pf = 62.4  # lbm/ft3
+u_pf = 1  # cp, viscosity of power fluid
+ppf_surf = 3046  # psi, power fluid surf pressure
+
+e42jetpump = JetPump('13', 'B')
+
+anlr_a, anlr_dhyd = anlr_dims(tube_od, case_id)
+q_pf = bpd_to_ft3s(bpd_pf)
+
+v_ann = round(q_pf/anlr_a, 2)  # ft/s annular velocity
+print(f'Annular Velocity: {v_ann} ft/s')
+
+rey = reynolds(d_pf, v_ann, anlr_dhyd, u_pf)
+print(f'Reynolds Number {rey}')
+f_ann = serghide(0.001, rey)
+print(f'Friction Factor {f_ann}')
+# add functionality of turbulent flow or not?
+dp_ann = dp_pipe(f_ann, jpump_md, d_pf, v_ann, anlr_dhyd)
+print(f'Friction Drop in Annulus {dp_ann} psi')
+
+p_st = static_press(d_pf, jpump_tvd)
+p_pf = ppf_surf + p_st - dp_ann
+
+print(f'Surf PF Pressure {ppf_surf} psi')
+print(f'Pump PF Pressure {round(p_pf,0)} psi')
+
+e42jetpump.p_jt(q_pf, p_pf, d_pf)
