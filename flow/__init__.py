d8d41c9a68816e74ed17421d6294f54da75a9418
Kaelin Ellis <ellis.kaelin@gmail.com>
2024-03-13 16:29:47 -0800

    more docstrings

---
+++ flow/__init__.py
@@ -0,0 +1,5 @@
+"""Flow Folder
+
+Collection of classes, functions and methods to describe flow inside pipes and jetpumps.
+Flow equations for single phase, two phase, reservoir inflow and jet pumps are present.
+"""
