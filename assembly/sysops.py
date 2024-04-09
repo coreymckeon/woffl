3d418f32f2b90508a3a53603798e054b6504dc4a
Kaelin Ellis <ellis.kaelin@gmail.com>
2024-04-09 15:31:49 -0800

    created system operations folder to house code for running jet pump system.

---
+++ assembly/sysops.py
@@ -0,0 +1,5 @@
+"""System Operations
+
+Code that has to do with running the entire system together. A combination of the IPR,
+PVT, JetPump and Outflow. Used to create a final solution to compare.
+"""
