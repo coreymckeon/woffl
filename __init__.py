bc66689476412256ddf3706ac5f6bbd36dfe74da
Kaelin Ellis <ellis.kaelin@gmail.com>
2024-04-10 16:20:25 -0800

    created pyproject.toml to use for shipping woffl

--- __init__.py
+++ __init__.py
@@ -5,3 +5,5 @@
 # https://peps.python.org/pep-0420/
 # https://docs.python.org/3/tutorial/modules.html
 # https://stackoverflow.com/questions/14132789/relative-imports-for-the-billionth-time
+
+__version__ = "1.0.0"
