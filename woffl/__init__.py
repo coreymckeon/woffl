cbb48c779f14233eb93e742f38910dc153e5f233
Kaelin Ellis <ellis.kaelin@gmail.com>
2024-04-12 08:36:25 -0800

    installed and using bumpver for pypi

--- woffl/__init__.py
+++ woffl/__init__.py
@@ -1,9 +1,1 @@
-# no idea what I should place in here
-
-# https://stackoverflow.com/questions/34753206/init-py-cant-find-local-modules
-# https://python-notes.curiousefficiency.org/en/latest/python_concepts/import_traps.html
-# https://peps.python.org/pep-0420/
-# https://docs.python.org/3/tutorial/modules.html
-# https://stackoverflow.com/questions/14132789/relative-imports-for-the-billionth-time
-
-__version__ = "1.0.0"
+__version__ = "1.0.1"  # bumpver automatically updates
