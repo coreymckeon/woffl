f33690055781e07b2dc768e4abf6334274f1c7df
Kaelin Ellis <ellis.kaelin@gmail.com>
2024-04-11 17:34:09 -0800

    rolled back version for pypi instead of test

--- woffl/pvt/__init__.py
+++ woffl/pvt/__init__.py
@@ -13,8 +13,3 @@ from .blackoil import BlackOil
 from .formgas import FormGas
 from .formwat import FormWater
 from .resmix import ResMix
-
-# https://stackoverflow.com/questions/34753206/init-py-cant-find-local-modules
-# https://python-notes.curiousefficiency.org/en/latest/python_concepts/import_traps.html
-# https://peps.python.org/pep-0420/
-# https://docs.python.org/3/tutorial/modules.html
