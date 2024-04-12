714b3c97fc7e6c38daa4b9464bf09f9517aa5375
Kaelin Ellis <ellis.kaelin@gmail.com>
2024-04-11 17:06:10 -0800

    modified pvt __init__ file to import blackoil, formgas and others

--- woffl/pvt/__init__.py
+++ woffl/pvt/__init__.py
@@ -4,12 +4,15 @@ Collection of classes, functions and methods for describing properties
 of hydrocarbon mixtures.
 
 ResMix (Class): Blah Blah
+BlackOil (Class):
+FormGas (Class):
+FormWater (Class):
 """
 
-# from .blackoil import BlackOil
-# from .formgas import FormGas
-# from .formwat import FormWater
-# from .resmix import ResMix
+from .blackoil import BlackOil
+from .formgas import FormGas
+from .formwat import FormWater
+from .resmix import ResMix
 
 # https://stackoverflow.com/questions/34753206/init-py-cant-find-local-modules
 # https://python-notes.curiousefficiency.org/en/latest/python_concepts/import_traps.html
