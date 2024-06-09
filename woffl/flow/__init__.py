3186ec68d0fc703accc9dd8ebe19e57a60974174
Kaelin Ellis <ellis.kaelin@gmail.com>
2024-06-09 14:14:41 -0800

    updated flow dunder init

--- woffl/flow/__init__.py
+++ woffl/flow/__init__.py
@@ -3,3 +3,5 @@
 Collection of classes, functions and methods to describe flow inside pipes and jetpumps.
 Flow equations for single phase, two phase, reservoir inflow and jet pumps are present.
 """
+
+from .inflow import InFlow
